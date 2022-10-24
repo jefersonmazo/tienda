e11d63e41c997624500ce78420300b12efcc8bd4
jefersonmazo <jefersonmazo1@gmail.com>
2022-10-24 14:42:24 -0500

    first commit

---
+++ Infrastructure/Data/Configuration/MarcaConfiguration.cs
@@ -0,0 +1,20 @@
+using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Data.Configuration;
+
+public class MarcaConfiguration : IEntityTypeConfiguration<Marca>
+{
+    public void Configure(EntityTypeBuilder<Marca> builder)
+    {
+        builder.ToTable("Marca");
+
+        builder.Property(p => p.Id)
+                .IsRequired();
+
+        builder.Property(p => p.Nombre)
+                .IsRequired()
+                .HasMaxLength(100);
+    }
+}
---
+++ Infrastructure/Data/Migrations/20221014154916_MarcasCategorias.cs
@@ -0,0 +1,190 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Data.Migrations
+{
+    public partial class MarcasCategorias : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Productos",
+                table: "Productos");
+
+            migrationBuilder.RenameTable(
+                name: "Productos",
+                newName: "Producto");
+
+            migrationBuilder.RenameColumn(
+                name: "precio",
+                table: "Producto",
+                newName: "Precio");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Precio",
+                table: "Producto",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(65,30)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Nombre",
+                table: "Producto",
+                type: "varchar(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<int>(
+                name: "CategoriaId",
+                table: "Producto",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "MarcaId",
+                table: "Producto",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Producto",
+                table: "Producto",
+                column: "Id");
+
+            migrationBuilder.CreateTable(
+                name: "Categorias",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Nombre = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categorias", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Marca",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Nombre = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Marca", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Producto_CategoriaId",
+                table: "Producto",
+                column: "CategoriaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Producto_MarcaId",
+                table: "Producto",
+                column: "MarcaId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Producto_Categorias_CategoriaId",
+                table: "Producto",
+                column: "CategoriaId",
+                principalTable: "Categorias",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Producto_Marca_MarcaId",
+                table: "Producto",
+                column: "MarcaId",
+                principalTable: "Marca",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Producto_Categorias_CategoriaId",
+                table: "Producto");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Producto_Marca_MarcaId",
+                table: "Producto");
+
+            migrationBuilder.DropTable(
+                name: "Categorias");
+
+            migrationBuilder.DropTable(
+                name: "Marca");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Producto",
+                table: "Producto");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Producto_CategoriaId",
+                table: "Producto");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Producto_MarcaId",
+                table: "Producto");
+
+            migrationBuilder.DropColumn(
+                name: "CategoriaId",
+                table: "Producto");
+
+            migrationBuilder.DropColumn(
+                name: "MarcaId",
+                table: "Producto");
+
+            migrationBuilder.RenameTable(
+                name: "Producto",
+                newName: "Productos");
+
+            migrationBuilder.RenameColumn(
+                name: "Precio",
+                table: "Productos",
+                newName: "precio");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "precio",
+                table: "Productos",
+                type: "decimal(65,30)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Nombre",
+                table: "Productos",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(100)",
+                oldMaxLength: 100)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Productos",
+                table: "Productos",
+                column: "Id");
+        }
+    }
+}
